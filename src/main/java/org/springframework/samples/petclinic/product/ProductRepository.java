c2eb01a908d2e8c52ae608675b3b505fa8a9acc1
raugalroc <raugalroc@alum.us.es>
2021-12-21 11:44:41 +0100

    Primeros cambios

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,13 +3,16 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product, String> {
     List<Product> findAll();
+    @Query("SELECT pt FROM ProductType pt")
     List<ProductType> findAllProductTypes();
+    
     Optional<Product> findById(int id);
     Product findByName(String name);
     Product save(Product p);
--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,9 +2,17 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+	
+	@Autowired
+	ProductRepository pr;
+	
     public List<Product> getAllProducts(){
-        return null;
+        return pr.findAll();
     }
 
     public List<Product> getProductsCheaperThan(double price) {
